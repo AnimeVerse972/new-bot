8f0ff2a37fc7d819c25a616445125873f6ab45cc
AnimeVerse972 <soibjonovdiyorbek0@gmail.com>
2025-06-16 08:00:45 +0500

    Update main.py

--- main.py
+++ main.py
@@ -102,7 +102,7 @@ async def handle_code(message: types.Message):
         "41": {"channel": "@AniVerseClip", "message_id": 835},
         "42": {"channel": "@AniVerseClip", "message_id": 864},
         "43": {"channel": "@AniVerseClip", "message_id": 918},
-        "44": {"channel": "@AniVerseClip", "message_id": 931},,
+        "44": {"channel": "@AniVerseClip", "message_id": 931},
         "45": {"channel": "@AniVerseClip", "message_id": },
         "46": {"channel": "@AniVerseClip", "message_id": },
         "47": {"channel": "@AniVerseClip", "message_id": },
